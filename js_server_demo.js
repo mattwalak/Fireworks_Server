5f5a6bbb9a641e15b8ce5bf8b94c4eb48b45f36a
Loaf! <walakmatthew@gmail.com>
2022-10-03 22:18:14 -0400

    server update

--- js_server_demo.js
+++ js_server_demo.js
@@ -1,11 +1,12 @@
+activeSky = null;
+
+
 const http = require('http');
 const ws = require('ws');
 
 const wss = new ws.Server({noServer: true});
 
 function accept(req, res) {
-  console.log("Incomming connection");
-
   // all incoming requests must be websockets
   if (!req.headers.upgrade || req.headers.upgrade.toLowerCase() != 'websocket') {
     console.log("Connection rejected because it is not a websocket");
@@ -20,34 +21,52 @@ function accept(req, res) {
     return;
   }
 
-  console.log("...accepting connection");
+  console.log("...accepting new connection");
   wss.handleUpgrade(req, req.socket, Buffer.alloc(0), onConnect);
 }
 
+function processDesignerMessage(ws, msg){
+  if(activeSky != null){
+    console.log("Designer: sending message to sky");
+    activeSky.send(msg);
+  }else{
+    console.log("Designer: no active sky");
+  }
+}
+
+function processSkyMessage(ws, msg){
+  if(msg === "setAsActiveSky"){
+    console.log("Sky: Setting this instance as active")
+    activeSky = ws;
+  }else{
+    console.log(`Sky: Unknown message = ${msg}`);
+  }
+}
+
+
 function onConnect(ws) {
   ws.on('message', function (message) {
+    console.log(`Got message ${message}`);
     message = message.toString();
-    console.log("Got message:");
-    console.log(message);
-
-    if(message === "Designer_Message"){
-      console.log("Got message from Firework Designer");
+    colon_i = message.indexOf(':');
+    if(colon_i < 0){
+      console.log(`Tossing message = ${message}; No delimiter`);
+    }else{
+      prefix = message.substring(0, colon_i);
+      data = message.substring(colon_i + 1, message.length)
+      if(prefix === "sky"){
+        processSkyMessage(ws, data);
+      }else if(prefix === "designer"){
+        processDesignerMessage(ws, data);
+      }else{
+        console.log(`Tossing message, unknown prefix = ${prefix}`)
+      }
     }
-
-    if(message === "Sky_Message"){
-      console.log("Got message from Sky");
-    }
-
-
-    /*
-    let name = message.match(/([\p{Alpha}\p{M}\p{Nd}\p{Pc}\p{Join_C}]+)$/gu) || "Guest";
-    ws.send(`Hello from server, ${name}!`);*/
-
-    // setTimeout(() => ws.close(1000, "Bye!"), 5000);
   });
 }
 
 if (!module.parent) {
+  console.log("launching on port 42742");
   http.createServer(accept).listen(42742);
 } else {
   exports.accept = accept;
